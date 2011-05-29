fa7bd65d0f341ab25f3ac979f8977634a85ac3ed
Jake Dahn <admin@jakedahn.com>
2011-05-28 23:51:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stsh::Application.config.secret_token = '5880e2ceb17df062d5bc3abbc01a6fd0c57e5e8b53cc27403a02f02b01cf256ba07fe5af1303ea4cb76547d811284fb1ec6b94d5cb73bb7c6091d28630615994'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Stsh::Application.config.session_store :cookie_store, :key => '_stsh_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Stsh::Application.config.session_store :active_record_store
