67158c6f838def8d6288590867a88ae1cec9a722
Jake Dahn <admin@jakedahn.com>
2011-07-10 03:22:24 -0700

    restyling and cleaning up a bit

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Stsh::Application.initialize!
+
+if ENV['RAILS_ENV'] == 'development'
+  ENV['RAILS_ASSET_ID'] = ''
+end
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,10 @@
 $(function(){
-
-});
+  document.onkeypress = function(evt) {
+     evt = evt || window.event;
+     var charCode = evt.which || evt.keyCode;
+     var charStr = String.fromCharCode(charCode);
+     if (evt.keyCode == 119) {
+       $("#wrapper").toggleClass("grid_on")
+     };
+  };
+});
